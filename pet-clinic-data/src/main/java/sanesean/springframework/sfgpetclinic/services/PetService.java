28545460e03c08d30cfa2fd818d0146f49d163f6
sean <sanesean@outlook.com>
2019-01-10 22:41:44 -0800

    Interfaces for services.

---
+++ pet-clinic-data/src/main/java/sanesean/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package sanesean.springframework.sfgpetclinic.services;
+
+import sanesean.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
