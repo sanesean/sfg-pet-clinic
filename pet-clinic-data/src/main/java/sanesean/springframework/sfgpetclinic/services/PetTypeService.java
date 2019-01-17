22b7e662bad54c0134178a4ce96520a472404420
sean <sanesean@outlook.com>
2019-01-17 13:41:44 -0800

    Add pettype and its service

---
+++ pet-clinic-data/src/main/java/sanesean/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package sanesean.springframework.sfgpetclinic.services;
+
+import sanesean.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
