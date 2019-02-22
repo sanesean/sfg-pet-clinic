8f87a69533c8f6cc4d521534eb12ecb71b0bcbc1
sean <sanesean@outlook.com>
2019-02-22 15:49:43 -0800

    add create/add new pet/pettype

--- pet-clinic-web/src/test/java/sanesean/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/sanesean/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -2,8 +2,10 @@ package sanesean.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import sanesean.springframework.sfgpetclinic.model.Owner;
@@ -23,7 +25,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
     @Mock
     OwnerService ownerService;
@@ -83,7 +85,7 @@ class OwnerControllerTest {
 
     @Test
     void initCreationForm() throws Exception {
-        mockMvc.perform(get("owners/new"))
+        mockMvc.perform(get("/owners/new"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"))
                 .andExpect(model().attributeExists("owner"));
