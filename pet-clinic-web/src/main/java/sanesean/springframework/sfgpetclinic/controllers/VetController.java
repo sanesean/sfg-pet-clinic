9f7854edf6d211825b2173d9bb37832985e8a9e6
sean <sanesean@outlook.com>
2019-03-02 14:56:45 -0800

    add vets list and json api

--- pet-clinic-web/src/main/java/sanesean/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sanesean/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,10 +3,15 @@ package sanesean.springframework.sfgpetclinic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sanesean.springframework.sfgpetclinic.model.Vet;
 import sanesean.springframework.sfgpetclinic.services.VetService;
 
-@RequestMapping({"vets", "/vets.html"})
+import java.util.Set;
+
+
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -16,9 +21,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/index"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
